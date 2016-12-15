99f7259592e9b3dfc0314184db3dc7d09dcc1d29
chinnani <ramnagesh.akubathini@gmail.com>
2016-12-15 17:24:16 +0530

    Adding to remote

---
+++ file1.js
@@ -0,0 +1,21 @@
+// Canvas Asteroids
+//
+// Copyright (c) 2010 Doug McInnes
+//
+
+KEY_CODES = {
+  32: 'ram',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  70: 'f',
+  71: 'g',
+  72: 'h',
+  77: 'm',
+  80: 'p'
+}
+--->>>git reset --hard ; git reset id ; git checkout master
+--->>>git log --graph --full-history --all --color \
+        --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"     For getting output for branches for visualisastion
+--->>>git log --graph --abbrev-commit --decorate --date=relative --all
---
+++ file2.js
@@ -0,0 +1,17 @@
+// Canvas Asteroids
+//
+// Copyright (c) 2010 Doug McInnes
+//
+
+KEY_CODES = {
+  32: 'space',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  70: 'f',
+  71: 'g',
+  72: 'h',
+  77: 'm',
+  80: 'p'
+}
